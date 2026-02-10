194e58eb32892efcedf2815890f5cdeb2562e292
Afnan <afnansaad445@gmail.com>
2026-02-11 01:18:56 +0200

    open boxes in main

--- interactive_book_app/lib/main.dart
+++ interactive_book_app/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:interactive_book_app/constants.dart';
 import 'package:interactive_book_app/screens/book_select_page.dart';
 
 import 'models/book_model.dart';
+import 'models/book_objects_model.dart';
+import 'models/content_model.dart';
 import 'models/glossary_model.dart';
 import 'models/keyword_model.dart';
 import 'models/toc_model.dart';
@@ -16,13 +18,13 @@ void main() async {
   Hive.registerAdapter(BookModelAdapter());
   await Hive.openBox(tocBox);
   Hive.registerAdapter(TocModelAdapter());
-  // await Hive.openBox(contentBox);
-  // Hive.registerAdapter(TocModelAdapter());
+  await Hive.openBox(contentBox);
+  Hive.registerAdapter(ContentModelAdapter());
   await Hive.openBox(glossaryBox);
   Hive.registerAdapter(GlossaryModelAdapter());
   await Hive.openBox(book_objectBox);
-  Hive.registerAdapter(TocModelAdapter());
-  await Hive.openBox(glossaryBox);
+  Hive.registerAdapter(bookobjectsAdapter());
+  await Hive.openBox(keywordsBox);
   Hive.registerAdapter(KeywordModelAdapter());
 
 }
