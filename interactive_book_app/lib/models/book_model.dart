ccc1aeea4ddaf1b5d497c0c62bb9ef5ffaae748c
shomos <shomoselshall6@gmail.com>
2026-02-10 22:33:45 +0200

    add hive package

--- interactive_book_app/lib/models/book_model.dart
+++ interactive_book_app/lib/models/book_model.dart
@@ -1,7 +1,6 @@
 import 'package:hive/hive.dart';
 
 //part 'book_model.g.dart';
-
 @HiveType(typeId: 0)
 class BookModel extends HiveObject {
   @HiveField(0)
@@ -9,20 +8,17 @@ class BookModel extends HiveObject {
   @HiveField(1)
   final String title;
   @HiveField(2)
-  final String author;
-  @HiveField(3)
   final String coverImageUrl;
-  @HiveField(4)
+  @HiveField(3)
   final String contentsURL;
-  @HiveField(5)
+  @HiveField(4)
   final String glossaryURL;
 
   BookModel({
     required this.id,
+    required this.title,
+    required this.coverImageUrl,
     required this.contentsURL,
     required this.glossaryURL,
-    required this.title,
-    required this.author,
-    required this.coverImageUrl,
   });
 }
